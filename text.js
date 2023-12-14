2b186135db36ff344ed44eaf8cbc67a498f2d902
6814@cbc.nsw.edu.au <6814@CBUS.LAN>
2023-12-14 15:10:25 +1100

    These are the changes made

---
+++ text.js
@@ -0,0 +1,9 @@
+
+// take input from the user
+num1 = prompt("Enter first number: ");
+num2 = prompt("Enter second number: ");
+num3 = prompt("Enter third number: ");
+largest = Math.max(num1, num2, num3);
+
+// display the result
+document.write(largest);
